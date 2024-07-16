6d3347f0a78de05de085d82ac1d52c51e188064e
bronifty <bronifty@gmail.com>
2024-07-16 14:08:19 -0400

    this

---
+++ cdk codes/file structure/app-stack.ts
@@ -0,0 +1,41 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+function getSuffixFromStack(stack: cdk.Stack) {
+  const shortStackId = cdk.Fn.select(2, cdk.Fn.split("/", stack.stackId));
+  const suffix = cdk.Fn.select(4, cdk.Fn.split("-", shortStackId));
+  return suffix;
+}
+
+class AppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // dynamically get suffix from stack
+    const suffix = getSuffixFromStack(this);
+    // CfnParameter
+    const myBucketName = new cdk.CfnParameter(this, "myBucketName", {
+      default: "bronifty-cdk-cloudfront-kv",
+    });
+
+    const deploymentBucket = new cdk.aws_s3.Bucket(this, "uiDeploymentBucket", {
+      bucketName: `${myBucketName.valueAsString}-${suffix}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    const table = new cdk.aws_dynamodb.Table(this, "notes", {
+      partitionKey: {
+        name: "noteId",
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const myBucket = new cdk.aws_s3.Bucket(this, "MyBucket", {
+      bucketName: myBucketName.valueAsString,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
--- cdk codes/file structure/app-stack.ts.md
+++ cdk codes/file structure/app-stack.ts.md
@@ -1,27 +1,27 @@
-
-```ts
+````ts
 export class AwsSdkJsNotesAppStack extends Stack {
 
 constructor(scope: Construct, id: string, props?: StackProps) {
 
 super(scope, id, props);
 
-  
+
 
 const table = new dynamodb.Table(this, "notes", {
-
 partitionKey: { name: "noteId", type: dynamodb.AttributeType.STRING },
-
-billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Set to pay-per-request
-
-removalPolicy: RemovalPolicy.DESTROY, // Ensure the table is destroyed when the stack is deleted
-
+billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+removalPolicy: RemovalPolicy.DESTROY,
 });
 
-  
+const myBucket = new cdk.aws_s3.Bucket(this, "MyBucket", {
+      bucketName: mySiteBucketName.valueAsString,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
 
 // ... existing code ...
 
 }
 
 }```
+````
